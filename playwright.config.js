0c4c280bf3c247c717cdb7eeb5f945fa4ae6cf10
Helen <helen_yrofeeva@epam.com>
2025-11-03 12:58:07 +0300

    iframe - locator api

--- playwright.config.js
+++ playwright.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. (undefined)*/
-  workers: process.env.CI ? 1 : 6,
+  workers: process.env.CI ? 1 : 8,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     // Standard line reporter for test progress
