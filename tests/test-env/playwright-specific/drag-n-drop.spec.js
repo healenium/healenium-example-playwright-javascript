d87be45812ead9b504293b9c348a3a6968239e17
alerof <alaerof@gmail.com>
2026-01-22 16:24:36 +0300

    Update locators based on Healenium report

--- tests/test-env/playwright-specific/drag-n-drop.spec.js
+++ tests/test-env/playwright-specific/drag-n-drop.spec.js
@@ -1,3 +1,4 @@
+
 import { test } from '@playwright/test';
 
 const TIMEOUT = 5000;
@@ -10,8 +11,8 @@ test.describe('Locator API - Drag Ang Drop - Tests', () => {
   test('source - playwright-specific', async ({ page }) => {
     test.slow();
 
-    const draggableElement = page.locator('.drag-container').getByText('Green Item');
-    const droppableElement = page.locator('#drop1');
+    const draggableElement = page.locator('div.drag-container > div.draggable-item.green-item');
+    const droppableElement = page.locator('//*[@id=\'new_drop_id\']');
     await draggableElement.dragTo(droppableElement, { timeout: TIMEOUT });
 
     // Click Change locators button to test healing
@@ -19,8 +20,8 @@ test.describe('Locator API - Drag Ang Drop - Tests', () => {
     await submitBtn.click();
 
     // Test healing - same action should work after locator change
-    const healedDraggableElement = page.locator('.drag-container').getByText('Green Item');
-    const healedDroppableElement = page.locator('#drop1');
+    const healedDraggableElement = page.locator('div.drag-container > div.draggable-item.green-item');
+    const healedDroppableElement = page.locator('//*[@id=\'new_drop_id\']');
     await healedDraggableElement.dragTo(healedDroppableElement, { timeout: TIMEOUT });
   });
 
@@ -28,7 +29,7 @@ test.describe('Locator API - Drag Ang Drop - Tests', () => {
     test.slow();
 
     const draggableElement = page.locator('//div[@class="drag-container"]/div[@name="dragRed"]');
-    const droppableElement = page.locator('#drop1');
+    const droppableElement = page.locator('//*[@id=\'new_drop_id\']');
     await draggableElement.dragTo(droppableElement, { timeout: TIMEOUT });
 
     // Click Change locators button to test healing
@@ -37,14 +38,14 @@ test.describe('Locator API - Drag Ang Drop - Tests', () => {
 
     // Test healing - same action should work after locator change
     const healedDraggableElement = page.locator('//div[@class="drag-container"]/div[@name="dragRed"]');
-    const healedDroppableElement = page.locator('#drop1');
+    const healedDroppableElement = page.locator('//*[@id=\'new_drop_id\']');
     await healedDraggableElement.dragTo(healedDroppableElement, { timeout: TIMEOUT });
   });
 
   test('target - playwright-specific', async ({ page }) => {
     test.slow();
 
-    const draggableElement = page.locator('.drag-container').getByText('Green Item');
+    const draggableElement = page.locator('div.drag-container > div.draggable-item.green-item');
     const droppableElement = page.getByTestId('testid_drop1');
     await draggableElement.dragTo(droppableElement, { timeout: TIMEOUT });
 
@@ -53,7 +54,7 @@ test.describe('Locator API - Drag Ang Drop - Tests', () => {
     await submitBtn.click();
 
     // Test healing - same action should work after locator change
-    const healedDraggableElement = page.locator('.drag-container').getByText('Green Item');
+    const healedDraggableElement = page.locator('div.drag-container > div.draggable-item.green-item');
     const healedDroppableElement = page.getByTestId('testid_drop1');
     await healedDraggableElement.dragTo(healedDroppableElement, { timeout: TIMEOUT });
   });
@@ -61,8 +62,8 @@ test.describe('Locator API - Drag Ang Drop - Tests', () => {
   test('target - CSS', async ({ page }) => {
     test.slow();
 
-    const draggableElement = page.locator('.drag-container').getByText('Green Item');
-    const droppableElement = page.locator('#drop1');
+    const draggableElement = page.locator('div.drag-container > div.draggable-item.green-item');
+    const droppableElement = page.locator('//*[@id=\'new_drop_id\']');
     await draggableElement.dragTo(droppableElement, { timeout: TIMEOUT });
 
     // Click Change locators button to test healing
@@ -70,15 +71,15 @@ test.describe('Locator API - Drag Ang Drop - Tests', () => {
     await submitBtn.click();
 
     // Test healing - same action should work after locator change
-    const healedDraggableElement = page.locator('.drag-container').getByText('Green Item');
-    const healedDroppableElement = page.locator('#drop1');
+    const healedDraggableElement = page.locator('div.drag-container > div.draggable-item.green-item');
+    const healedDroppableElement = page.locator('//*[@id=\'new_drop_id\']');
     await healedDraggableElement.dragTo(healedDroppableElement, { timeout: TIMEOUT });
   });
 
   test('source and target - playwright-specific both', async ({ page }) => {
     test.slow();
 
-    const draggableElement = page.locator('.drag-container').getByText('Green Item');
+    const draggableElement = page.locator('div.drag-container > div.draggable-item.green-item');
     const droppableElement = page.getByTestId('testid_drop1');
     await draggableElement.dragTo(droppableElement, { timeout: TIMEOUT });
 
@@ -87,7 +88,7 @@ test.describe('Locator API - Drag Ang Drop - Tests', () => {
     await page.locator('#Submit_checkbox').click();
 
     // Test healing - same action should work after locator change
-    const healedDraggableElement = page.locator('.drag-container').getByText('Green Item');
+    const healedDraggableElement = page.locator('div.drag-container > div.draggable-item.green-item');
     const healedDroppableElement = page.getByTestId('testid_drop1');
     await healedDraggableElement.dragTo(healedDroppableElement, { timeout: TIMEOUT });
   });
@@ -96,7 +97,7 @@ test.describe('Locator API - Drag Ang Drop - Tests', () => {
     test.slow();
 
     const draggableElement = page.locator('//div[@class="drag-container"]/div[@name="dragRed"]');
-    const droppableElement = page.locator('#drop1');
+    const droppableElement = page.locator('//*[@id=\'new_drop_id\']');
     await draggableElement.dragTo(droppableElement, { timeout: TIMEOUT });
 
     // Click Change locators button to test healing
@@ -105,7 +106,7 @@ test.describe('Locator API - Drag Ang Drop - Tests', () => {
 
     // Test healing - same action should work after locator change
     const healedDraggableElement = page.locator('//div[@class="drag-container"]/div[@name="dragRed"]'); 
-    const healedDroppableElement = page.locator('#drop1');
+    const healedDroppableElement = page.locator('//*[@id=\'new_drop_id\']');
     await healedDraggableElement.dragTo(healedDroppableElement, { timeout: TIMEOUT });
   });  
 
@@ -129,8 +130,8 @@ test.describe('Locator API - Drag Ang Drop - Tests', () => {
   test('source and target - playwright-specific, CSS', async ({ page }) => {
     test.slow();
 
-    const draggableElement = page.locator('.drag-container').getByText('Green Item');
-    const droppableElement = page.locator('#drop1');
+    const draggableElement = page.locator('div.drag-container > div.draggable-item.green-item');
+    const droppableElement = page.locator('//*[@id=\'new_drop_id\']');
     await draggableElement.dragTo(droppableElement, { timeout: TIMEOUT });
 
     // Click Change locators button to test healing
@@ -138,8 +139,8 @@ test.describe('Locator API - Drag Ang Drop - Tests', () => {
     await page.locator('#Submit_checkbox').click();
 
     // Test healing - same action should work after locator change
-    const healedDraggableElement = page.locator('.drag-container').getByText('Green Item');
-    const healedDroppableElement = page.locator('#drop1');
+    const healedDraggableElement = page.locator('div.drag-container > div.draggable-item.green-item');
+    const healedDroppableElement = page.locator('//*[@id=\'new_drop_id\']');
     await healedDraggableElement.dragTo(healedDroppableElement, { timeout: TIMEOUT });
   }); 
 
@@ -147,14 +148,14 @@ test.describe('Locator API - Drag Ang Drop - Tests', () => {
   test('page.dragAndDrop', async ({ page }) => {
     test.slow();
 
-    await page.dragAndDrop('//div[@class="drag-container"]/div[@name="dragRed"]', '#drop1', { timeout: TIMEOUT });
+    await page.dragAndDrop('//div[@class="drag-container"]/div[@name="dragRed"]', '//*[@id=\'new_drop_id\']', { timeout: TIMEOUT });
 
     // Click Change locators button to test healing
     await page.locator('#Submit').click();
     await page.locator('#Submit_checkbox').click();
 
     // Test healing - same action should work after locator change
-    await page.dragAndDrop('//div[@class="drag-container"]/div[@name="dragRed"]', '#drop1', { timeout: TIMEOUT });
+    await page.dragAndDrop('//div[@class="drag-container"]/div[@name="dragRed"]', '//*[@id=\'new_drop_id\']', { timeout: TIMEOUT });
   });  
 
 });
